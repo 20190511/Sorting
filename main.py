f0cf24f0ea119834a2e285106be049eebcfc16fa
배준형 <70988272+20190511@users.noreply.github.com>
2022-12-30 10:00:44 +0000

    QuickSort 방식 추가

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ def quickSort (array, start, end):
   quickSort(array,right+1,end)
 
 
+def quickSort2 (array):
+  if len(array)<=1:
+    return array
+  pivot = array[0]
+  tail = array[1:]
+
+  left_side = [x for x in tail if x <= pivot]
+  right_side = [x for x in tail if x > pivot]
+  return quickSort2(left_side)+[pivot]+quickSort2(right_side)
+
+
 
 a = [5,7,9,0,3,1,6,2,4,8]
+b = a.copy()
 quickSort(a,0,len(a)-1)
+b = quickSort2(b)
+
 print(a)
+print(b)
