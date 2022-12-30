4e9afcc73aead8825a8e66403b1191883086e8a2
배준형 <70988272+20190511@users.noreply.github.com>
2022-12-30 10:20:10 +0000

    계수정렬 O(k+N)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def RadixSort(array):
     for j in range (temp[i]):
       newList.append(i)
   return newList
-array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+array = [.7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
 print(RadixSort(array))
 
     
